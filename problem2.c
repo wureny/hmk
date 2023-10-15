65b9a64e8448a11f47120027f28353a505891dcc
wureny <112567966+wureny@users.noreply.github.com>
2023-10-15 19:09:29 +0800

    Update problem2.c
    fix: del_substr should only delete the first substr.

--- problem2.c
+++ problem2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
 int del_substr(char *str, const char *substr) {
     if (str == NULL || substr == NULL || *substr == '\0') {
         return 0;
     }
-    size_t str_len = strlen(str);
     size_t substr_len = strlen(substr);
     if (substr_len == 0) {
         return 0; 
@@ -12,16 +13,20 @@ int del_substr(char *str, const char *substr) {
     char *destination = str; 
     char *match;
 
-    while ((match = strstr(source, substr)) != NULL) {
+    // 使用strstr查找substr在str中的第一个出现
+    if ((match = strstr(source, substr)) != NULL) {
         size_t prefix_len = match - source; 
         for (size_t i = 0; i < prefix_len; i++) {
             *destination++ = *source++;
         }
-
-        source += substr_len; // Move the source pointer past the match
+        source += substr_len; // 移动source指针到匹配之后
     }
+
+    // 复制剩下的字符
     while ((*destination++ = *source++) != '\0');
+
     return 1;
 }
+
 
 
